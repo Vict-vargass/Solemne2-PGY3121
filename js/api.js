c983865f099027a14e82c67340e2c67630d63439
victor vargas <vict.vargass@duocuc.cl>
2021-05-29 15:53:07 -0400

    primeros archivos"

---
+++ js/api.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('#btnEnviar').submit(function(){
+        alert("Formulario enviado")
+    })
+    $('#rut').select(function(){
+        console.log("rut seleccionado")
+    })
+    $('#pas').click(function(){
+        console.log("pasaporte elegido")
+    })
+})//fin document ready{
+
+
