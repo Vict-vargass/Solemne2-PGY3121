31e120267a1fee16ab304e01bfef0d09a1f267ec
victor vargas <vict.vargass@duocuc.cl>
2021-06-06 19:12:49 -0400

    desactiar buscar si no hay campos

--- js/api.js
+++ js/api.js
@@ -8,9 +8,11 @@ $(document).ready(function(){
     //REVISA SI EL CAMPO ESTA COMPLETO, MANDA UNA ALERTA QUE MARCA EL CAMPO INCOMPLETO Y OTORGA EL VALOR A LA VARIABLE INICIALIZADA
     $('#nombreArtista').blur(function(){
         if ($('#nombreArtista').val()==""){
-            $('#nombreArtista').addClass('alerta');   
+            $('#nombreArtista').addClass('alerta');
+            $("#traerSpotify").prop('disabled', true);   
         }else{
             $('#nombreArtista').removeClass('alerta');
+            $("#traerSpotify").prop('disabled', false);
             nombreArtista=$('#nombreArtista').val();
         }
     })
@@ -18,9 +20,19 @@ $(document).ready(function(){
     $('#token').blur(function(){
         if($('#token').val()==""){
             $('#token').addClass('alerta');
+            $("#traerSpotify").prop('disabled', true);
         }else{
             $('#token').removeClass('alerta');
+            $("#traerSpotify").prop('disabled', false);
             tokenAsignado=$('#token').val();
+        }
+    })
+    //COMPRUEBA CAMPOS Y DESACTIVA BOTON SI ESTAN VACIOS
+    $('#traerSpotify').hover(function(){
+        if(tokenAsignado==""||nombreArtista==""){
+            $("#traerSpotify").prop('disabled', true);
+        }else{
+            $("#traerSpotify").prop('disabled', false);
         }
     })
     //TRAE API DE SPOTIFY
@@ -53,6 +65,7 @@ $(document).ready(function(){
         contenedor.empty();
          $('#nombreArtista').val("").removeClass('alerta');
          $('#token').removeClass('alerta');
+         $("#traerSpotify").prop('disabled', false);
     })
 })//fin document ready{
 
